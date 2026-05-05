581c342082a37cbc47e5962b2e7c9337bf4d22f9
ken-rmndbn <remandabankenneth29@gmail.com>
2026-05-05 22:11:13 +0800

    creating code for math operations

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -1,2 +1,15 @@
 class MathOperations:
-    def
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("Cannot divide number by zero!")
+        return a / b
+    
